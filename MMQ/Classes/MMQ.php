1b60d3f5a4a2a8ba8f0ade0d8caf9aed16f4d6d8
Hylson Kobayashi <hylsonk@gmail.com>
2017-11-25 14:47:25 -0200

    projeto consertado

--- MMQ/Classes/MMQ.php
+++ MMQ/Classes/MMQ.php
@@ -14,6 +14,21 @@ use NumPHP\LinAlg\LinAlg;
 
 class MMQ
 {
+
+    /**
+     * Função que faz o calculo da Matriz A apartir do vetor dos valores das coordenadas X dos pontos
+     *
+     * A = [x0 1]
+     *     [x1 1]
+     *     [x2 1]
+     *     [x3 1]
+     *     [x4 1]
+     *
+     *
+     * @param NumArray $vector
+     * @param int $grau
+     * @return NumArray
+     */
     public function calculaA(NumArray $vector, $grau = 1){
         $matrixA = NumPHP::zeros($vector->getSize(),$grau+1);
         for ($i = 0; $i <$vector->getSize();$i++){
@@ -26,37 +41,64 @@ class MMQ
             }
 
         }
-//        echo $matrixA;
         return $matrixA;
     }
 
-    public function fazMMQ(NumArray $vectorX,NumArray $vectorY, $grau = 1){
+
+    /**
+     * Faz o metodo de Minimos Quadrados apartir da entrada dos valores das coordenadas X e Y.
+     * Como o metodo é feito de uma função exponencial é necessario fazer a coversão da formula Y = Ae^(Bx) para ln(Y) = Ln(A) +Bx
+     *
+     * @param NumArray $vectorX
+     * @param NumArray $vectorY
+     * @param int $grau
+     * @return NumArray
+     */
+    public function fazMMQ(NumArray $vectorX, NumArray $vectorY, $grau = 1){
+//        Transforma o vetorX numa Matriz de potencias
         $matrizA = $this->calculaA($vectorX,$grau);
 
+//        Faz Ln(Y)
         $lnY = \NumPHP\Core\NumPHP::zeros($vectorY->getSize());
         for ($i = 0 ; $i < $vectorY->getSize(); $i++){
             $lnY->set($i, log($vectorY->get($i)->getData()));
         }
 
+//        Multiplica a matrizA e os seus pontos correspondentes Ln(Y) pela transposta da matrizA gerando TA e TB
         $TA = $matrizA->getTranspose();
         $TB = $matrizA->getTranspose();
         $TA->dot($matrizA);
         $TB->dot($lnY);
 
-        $lin = LinAlg::solve($TA,$TB);
-//        echo "oi";
+//        Resolve o sistema linear TA x = TB
+        $lin = LinAlg::solve($TA,$TB);;
+
+//        Antes de devolver o vetor de coeficientes encontrado no sistema linear precisa fazer o passo inverso de Ln que foi usado na conversão
         $lin->set(1,exp($lin->get(1)->getData()));
-//        echo $lin;
+
         return $lin;
     }
 
-    public function geraPontosDaFuncao(NumArray $coef,$numPontos){
+
+    /**
+     * Função que retorna os valores da função encontrada com os coeficientes retornados do fazMMQ
+     * O padrão de retorno é um vetor onde a posição corresponde ao valor de X e o valor de Y
+     *
+     * Y = A*e^(B*X)
+     * pontos[X] = Y;
+     *
+     * Sendo que X precisa está num intervalo de 0 à $numPontos
+     *
+     * O retorno é um JSON dos pontos que será consumido pelo aplicativo
+     * @param NumArray $coef
+     * @param $numPontos
+     */
+    public function geraPontosDaFuncao(NumArray $coef, $numPontos){
         for ($i = 0;$i < $numPontos; $i++){
             $Y = $coef->get(1)->getData()*exp($i*$coef->get(0)->getData());
             $pontos[$i] = $Y;
         }
-//        print $coef->get(0)."*X + ".$coef->get(1);
+
         echo json_encode($pontos);
-//        return $pontos;
     }
 }
--- MMQ/index.php
+++ MMQ/index.php
@@ -5,112 +5,42 @@ require_once("vendor/autoload.php");
  * User: Kobayashi
  * Date: 12/11/2017
  * Time: 18:56
+ *
+ * O projeto tem o objetivo de usar o metodo de minimos quadrados com dados de periodos e quantas disciplinas foram
+ * concluidas por todos so alunos que usam o aplicativo Minha Grade. O resultado desse metodo será exibido numa area
+ * do aplicativo onde o usuario terá um auxilio visual(grafico) de como ele está em relação aos demais alunos
+ *
+ * Para auxiliar as operações com matrizes foi usado uma biblioteca chamada NumPHP que possui limitações de funcionalidades
  */
 
-//$X = [1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,5,5,5,5,5,5,6,6,6,7,7,7,8,8,9,9,10,10,11,12,13,14];
-//$Y = [5,4,3,9,10,8,7,14,12,16,11,9,18,13,21,11,23,13,26,15,19,11,14,23,11,15,26,12,16,15,16,18,19,21,22,26,17,31];
 
+
+//Chama a Classe MMQ
 $mmq = new \Classes\MMQ();
 
+//Vetor com os valores das coordenadas x que correspondem aos periodos
 $vectorA = new \NumPHP\Core\NumArray(
 //    [1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,5,5,5,5,5,5,6,6,6,7,7,7,8,8,9,9,10,10,11,12,13,14]
-[1,
-1,
-1,
-1,
-1,
-1,
-2,
-2,
-2,
-2,
-2,
-2,
-3,
-3,
-3,
-3,
-3,
-3,
-4,
-4,
-4,
-4,
-4,
-4,
-5,
-5,
-5,
-5,
-5,
-5,
-6,
-6,
-6,
-7,
-7,
-7,
-8,
-8,
-9,
-9,
-10,
-10,
-11,
-12,
-13,
-14]
+    [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4,
+        4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 9, 9, 10, 10,
+        11, 12, 13, 14]
 );
 
+
+
+//Vetor com os valores das coordenadas y que correspondem a quantidade de disciplinas concluidas correspondente aos periodos no Vetor A
 $vectorB = new \NumPHP\Core\NumArray(
 //    [5,4,3,9,10,8,7,14,12,16,11,9,18,13,21,11,23,13,26,15,19,11,14,23,11,15,26,12,16,15,16,18,19,21,22,26,17,31]
-[5,
-5,
-3,
-4,
-4,
-5,
-9,
-9,
-7,
-8,
-8,
-10,
-14,
-12,
-9,
-12,
-11,
-16,
-18,
-13,
-11,
-13,
-13,
-21,
-23,
-13,
-11,
-19,
-15,
-26,
-14,
-11,
-23,
-15,
-12,
-26,
-16,
-15,
-16,
-18,
-19,
-21,
-22,
-26,
-27,
-31]
+    [5, 5, 3, 4, 4, 5, 9, 9, 7, 8, 8, 10, 14, 12, 9, 12, 11, 16, 18, 13,
+        11, 13, 13, 21, 23, 13, 11, 19, 15, 26, 14, 11, 23, 15, 12, 26, 16,
+        15, 16, 18, 19, 21, 22, 26, 27, 31]
 );
 
+
+
+//Calcula os coeficientes da função relativa gerada pelo MMQ
 $coef = $mmq->fazMMQ($vectorA,$vectorB,1);
-$mmq->geraPontosDaFuncao($coef,15);
+echo $coef;
+
+//Executa a função apartir dos coeficientes calculados retornando um numero de pontos
+$mmq->geraPontosDaFuncao($coef,20);
